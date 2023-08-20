3eb4d9b58f44a0127f0b45c467ac3c620d723fbd
watase.atsushi <beechan920@yahoo.co.jp>
2023-08-20 15:06:08 +0900

    FE:登録ボタンの追加

--- client/src/pages/VersionManagement.tsx
+++ client/src/pages/VersionManagement.tsx
@@ -1,5 +1,25 @@
 const VersionManagement = () => {
-  return <h1 className="text-xl font-bold">バージョン管理</h1>;
+  return (
+    <>
+      <h1 className="text-xl font-bold">バージョン管理</h1>
+      <button className="btn btn-circle btn-outline">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 4.5v15m7.5-7.5h-15"
+          />
+        </svg>
+      </button>
+    </>
+  );
 };
 
 export default VersionManagement;
