610319d5292717fb8c437abe2f0b622e12d59c73
watase.atsushi <beechan920@yahoo.co.jp>
2023-10-27 23:37:06 +0900

    FE:社員登録画面にアイコンイメージを追加

--- client/src/pages/NewEmployee.tsx
+++ client/src/pages/NewEmployee.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 import { useMutation, useQuery } from "@apollo/client";
 import VersionSelectBox from "@/components/version/VersionSelectBox";
 import { DepartmentOptions } from "@/functions/department.ts";
@@ -99,6 +100,18 @@ const NewEmployee = () => {
     return ancestry;
   };
 
+  const editImage = () => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    });
+  };
+
   const notLoadingError: boolean =
     !loading && !error && !versionError && !versionLoading;
 
@@ -110,6 +123,24 @@ const NewEmployee = () => {
         versions != undefined &&
         selectedVersionId != undefined && (
           <>
+            <div onClick={editImage}>
+              <figure className="px-10 pt-10">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-24 h-24 cursor-pointer"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+                  />
+                </svg>
+              </figure>
+            </div>
             <div className="mt-5">
               <label>姓</label>
               <br />
